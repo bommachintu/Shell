fc8857b179bff3003f216af28f47e6960456ecc6
BommaRahul <bommachintu@gmail.com>
2020-03-29 12:25:35 -0300

    operations on num

--- arithmaric.sh
+++ arithmaric.sh
@@ -1,7 +1,14 @@
-i#! /bin/bash
+#! /bin/bash
 num1=3.10
 num2=30
 echo $num1 + $num2 | bc
 
 
 echo "20.5 + 30" | bc
+
+read num3
+read num4
+echo addition =  $((num3+num4))
+echo subtraction = $((num3-num4))
+echo multiplication = $((num3*num4))
+echo Division = $((num3/num4))
